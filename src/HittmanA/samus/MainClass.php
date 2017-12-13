3998ff8acfeb88354b0e8541b016611059f417b3
HittmanA <hittmana@comcast.net>
2017-12-12 23:27:14 -0500

    Added a basic MainClass file and config file

---
+++ src/HittmanA/samus/MainClass.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace HittmanA\samus;
+
+//PocketMine
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\event\Listener;
+use pocketmine\Player;
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\Config;
+use pocketmine\utils\TextFormat;
+use pocketmine\event\player\PlayerJoinEvent;
+
+class MainClass extends PluginBase implements Listener
+{
+
+    /** @var Config */
+    protected $users;
+
+    $users = new Config($this->getDataFolder() . "players.json", Config::JSON, []);
+
+    public function onEnable()
+    {
+        //Make the faction config
+        @mkdir($this->getDataFolder());
+        $this->saveDefaultConfig();
+
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        $this->getServer()->getPluginManager()->registerEvents(new Events($this), $this);
+
+        $this->getLogger()->notice("Loaded!");
+    }
+
+    public function onDisable()
+    {
+        $this->getLogger()->info(TextFormat::GREEN . "Unloading!");
+    }
+
+    public function onJoin(PlayerJoinEvent $event)
+    {
+        $player = $event-getPlayer();
+        if(isset($users->get($player->getName()))) {
+            $player->sendMessage("Welcome back");
+        } else {
+            $users->set(strtolower($sender->getName()), [
+                "name" => strtolower($sender->getName()),
+                "password" => "IMAPASSWORD"
+            ]);
+            $users->save();
+        }
+    }
+
+}
